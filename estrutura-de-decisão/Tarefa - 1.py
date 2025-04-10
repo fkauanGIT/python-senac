ab2c82ba08d5fd12b915e3cbce0ab9f069bb1234
fkauanGIT <kauanuniversity@gmail.com>
2025-04-09 21:46:10 -0300

    Alterações nos arquivos Tarefa - 1.py e Tarefa - 2.py

---
+++ estrutura-de-decisão/Tarefa - 1.py
@@ -0,0 +1,7 @@
+nota = float(input("Digite sua nota: "))
+
+while nota >= 0 and nota <= 10:
+    print("Nota OK!")
+    break
+else:
+    print("Nota Inválida !")
---
+++ estrutura-de-decisão/Tarefa - 2.py
@@ -0,0 +1,9 @@
+while True:
+    nome = input("Digite sua nota: ")
+    senha = input("Digite sua nota: ")
+
+    if nome == senha:
+        print("Senha inválida")
+    else:
+        print("Login aceito")
+        break
