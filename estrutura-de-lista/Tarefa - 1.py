7d66d54c6daa053355f8f81bc0a512c892e27611
fkauanGIT <kauanuniversity@gmail.com>
2025-04-11 21:27:04 -0300

    tchau

--- estrutura-de-lista/Tarefa - 1.py
+++
@@ -1,9 +0,0 @@
-vetor = []
-
-for i in range(5):
-    vetor.append(int(input("Digite um número inteiro: ")))
-
-vetor.sort()
-
-print("Números em ordem crescente:", vetor)
-print("Quantidade de números digitados:", len(vetor))
