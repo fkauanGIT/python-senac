54f6a08a09c55b113b5b1a46f703707d7fcd4cbb
fkauanGIT <kauanuniversity@gmail.com>
2025-04-14 21:30:44 -0300

    tchau

--- estrutura-de-funcoes/Tarefa - 1.py
+++
@@ -1,8 +0,0 @@
-def print_pattern(number):
-    for i in range(1, number + 1):
-        line = (str(i) + ' ') * i
-        print(line.strip())
-
-
-n = int(input("Enter a number: "))
-print_pattern(n)
